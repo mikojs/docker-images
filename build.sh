736142c514e3b21775765faa1b2fbee6de16bf5b
HsuTing <hsuting0106@gmail.com>
2022-05-25 02:26:04 +0800

    perf(build): could filter images with arguments and add more information

--- build.sh
+++ build.sh
@@ -1,17 +1,39 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+  files=$(ls)
+else
+  files=$@
+fi
+
 if [ ! -d config ]; then
   mkdir config
 fi
 
-for file in $(ls); do
+print_sign() {
+  i=1
+  while [ $i -le $2 ]; do
+    printf $1
+    i=$((i + 1))
+  done
+}
+
+for file in $files; do
   if [ -f $file/Dockerfile ]; then
     if [ ! -d config/$file ]; then
       mkdir config/$file
     fi
 
-    echo "[$file] start"
+    printf "┌"
+    print_sign "─" $((${#file} + 10 ))
+    printf "┐\n"
+
+    echo "│     $file     │"
+
+    printf "└"
+    print_sign "─" $((${#file} + 10 ))
+    printf "┘\n"
+
     docker build -t $file -f ./$file/Dockerfile .
-    echo "[$file] end"
   fi
 done
