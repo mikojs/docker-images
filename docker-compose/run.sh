f38cd583624d9a6d6f468afc76c343ea29b53c67
HsuTing <hsuting0106@gmail.com>
2022-05-27 02:02:37 +0800

    refactor(docker-compose): remove run script

--- docker-compose/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f ./.env ]; then
-  for env_name in NGROK_AUTHTOKEN SLACK_BOT_TOKEN SLACK_BOT_CHANNEL; do
-    echo "$env_name=$(printenv $env_name)" >> .env
-  done
-fi
-
-docker-compose -f ./run.yml up -d
