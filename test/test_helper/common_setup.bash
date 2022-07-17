01cc7c1695dfef9a290dfeae146cd2c3976aa62d
HsuTing <hsuting0106@gmail.com>
2022-07-17 16:56:26 +0000

    feat(test): add common_setup

---
+++ test/test_helper/common_setup.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+local dir=$(cd $(dirname $BATS_TEST_FILENAME) >/dev/null 2>&1 && pwd)
+local filename=$(basename $BATS_TEST_FILENAME | sed 's/.bats/.sh/g')
+
+source "$dir/../$filename"
