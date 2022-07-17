6c808fa20422c139bb4bb0b3c6749d0b9580dbe0
HsuTing <hsuting0106@gmail.com>
2022-07-17 16:39:02 +0000

    feat(dind): add dind shell

--- dind.sh
+++ dind.sh
@@ -1,5 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-# need to install shc: apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/testing shc
-# compile: shc -f ./dind.sh && mv ./dind.sh.x ./dind
-echo test
+check_volume_name() {
+  if [[ -z $VOLUME_NAME ]]; then
+    printf "couldn't find the volume name"
+    exit 1
+  fi
+}
+
+get_work_dir() {
+  if [[ $(pwd) =~ ^/project ]]; then
+    pwd
+  else
+    printf "/project"
+  fi
+}
+
+if [[ -z $BATS_TEST_FILENAME ]]; then
+  check_volume_name
+
+  docker run \
+    -it \
+    -v $VOLUME_NAME:/project \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -e VOLUME_NAME=$VOLUME_NAME \
+    -w $(get_work_dir) \
+    $@
+fi
