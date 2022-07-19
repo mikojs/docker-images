3eb357e0a4537b98e1bb64fbc3e2fae49797e771
HsuTing <hsuting0106@gmail.com>
2022-07-19 13:16:57 +0000

    perf(dind): add remove functions

--- dind.sh
+++ dind.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-get_work_dir() {
-  if [[ $(pwd) =~ ^/project ]]; then
-    pwd
-  else
-    printf "/project"
+remove_stopped_container() {
+  local stopped_containers=($(docker ps -aq -f status=exited | tr '\n' ' '))
+
+  if [[ ${#stopped_containers[@]} != 0 ]]; then
+    docker rm ${stopped_containers[*]}
   fi
 }
 
-if [[ -z $BATS_TEST_FILENAME ]]; then
+remove_none_images() {
+  local none_images=($(docker images -q -f dangling=true | tr '\n' ' '))
+
+  if [[ ${#none_images[@]} != 0 ]]; then
+    docker rmi ${none_images[*]}
+  fi
+}
+
+run_container() {
+  local work_dir
+
+  if [[ $(pwd) =~ ^/project ]]; then
+    work_dir=$(pwd)
+  else
+    work_dir="/project"
+  fi
+
   docker run \
     -it \
     --volumes-from $(cat /etc/hostname) \
-    -w $(get_work_dir) \
+    -w $work_dir \
     $@
-fi
+}
