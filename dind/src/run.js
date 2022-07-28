3c76eb247dcd71eaab0f1cc9818a5bb921f670b4
HsuTing <hsuting0106@gmail.com>
2022-07-28 07:59:37 +0000

    feat(dind): could run container with dind

--- dind/src/run.js
+++ dind/src/run.js
@@ -1,13 +1,37 @@
-import { Command } from 'clipanion';
+import fs from 'fs';
+
+import { Command, Option } from 'clipanion';
 import spawn from 'cross-spawn';
+
+const HOSTNAME_FILE_PATH = '/etc/hostname';
 
 export default class Run extends Command {
   static paths = [['run'], Command.Default];
 
+  args = Option.Proxy();
+
   async execute() {
     const { stdin, stdout, stderr } = this.context;
+    const args = [
+      'run',
+      '-it',
+      '-w',
+      /^\/project/.test(process.cwd())
+        ? process.cwd()
+        : '/project',
+    ];
 
-    await spawn.sync('docker', {
+    if (fs.existsSync(HOSTNAME_FILE_PATH))
+      args.push(
+        '--volumes-from',
+        fs.readFileSync(HOSTNAME_FILE_PATH, 'utf-8')
+          .replace(/\n/g, ''),
+      );
+
+    await spawn.sync('docker', [
+      ...args,
+      ...this.args,
+    ], {
       stdio: [stdin, stdout, stderr],
     });
   }
