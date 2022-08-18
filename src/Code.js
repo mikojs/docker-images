656e6c81510102803a4a3cb0bd7ea62a96a99c54
HsuTing <hsuting0106@gmail.com>
2022-08-18 08:16:27 +0000

    feat(code): would ask for creating the file

--- src/Code.js
+++ src/Code.js
@@ -1,7 +1,9 @@
+import fs from 'fs';
 import path from 'path';
 
 import { Command, Option } from 'clipanion';
 import glob from 'glob';
+import { Confirm } from 'enquirer';
 import spawn from 'cross-spawn';
 
 import getStdio from './utils/getStdio';
@@ -20,24 +22,34 @@ export default class Code extends Command {
 
   args = Option.Proxy();
 
+  conformToCreateFile = async relativePath => {
+    const answer = await new Confirm({
+      name: 'question',
+      message: `Couldn't find \`${relativePath}\`. Do you want to create this?`,
+    }).run();
+
+    if (!answer) return [];
+
+    const filePath = path.resolve(process.cwd(), relativePath);
+
+    fs.writeFileSync(filePath, '\n', 'utf-8');
+
+    return [filePath];
+  };
+
   execute = async () => {
-    const files = this.args
-      .map(pattern => {
+    const files = (await Promise.all(
+      this.args.map(async pattern => {
         const result = glob.sync(pattern, { nodir: true, nonull: true, absolute: true });
 
-        if (result.length === 1 && result[0] === pattern) {
-          if (/\*/.test(result[0]))
-            return [];
-
-          const filePath = path.resolve(process.cwd(), result[0]);
-
-          // TODO: should ask for creating or not
-          return [filePath];
-        }
+        if (result.length === 1 && result[0] === pattern)
+          return /\*/.test(pattern)
+            ? []
+            : await this.conformToCreateFile(pattern);
 
         return result;
       })
-      .flat();
+    )).flat();
 
     await spawn.sync('code-server', files, getStdio(this.context));
   };
