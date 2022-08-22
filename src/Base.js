2cc833d378278bae4947c68eeb37fc6472d8180e
HsuTing <hsuting0106@gmail.com>
2022-08-22 13:38:41 +0000

    feat(base): add execResult

--- src/Base.js
+++ src/Base.js
@@ -2,6 +2,9 @@ import spawn from 'cross-spawn';
 import { Command } from 'clipanion';
 
 export default class Base extends Command {
+  execResult = async (command, ...args) =>
+    spawn.sync(command, args);
+
   exec = async (command, ...args) => {
     const { stdin, stdout, stderr } = this.context;
 
