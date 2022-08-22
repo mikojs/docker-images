7b9770926341e92c066338f4cf35fd6a47308caa
HsuTing <hsuting0106@gmail.com>
2022-08-22 15:43:08 +0000

    perf(rmi): doesn't use remove function

--- src/Rmi.js
+++ src/Rmi.js
@@ -1,15 +1,14 @@
 import { Command } from 'clipanion';
 
 import Base from './Base';
-import remove from './utils/remove';
 
 export default class Rmi extends Base {
   static paths = [['rmi']];
 
   static usage = Command.Usage({
-    description: 'Remove the none images',
+    description: 'Remove the none-images',
     details: `
-      This command would find the ids of the none images and remove them.
+      This command would find the ids of the none-images and remove them.
     `,
     examples: [[
       'Remove the none images',
@@ -17,5 +16,19 @@ export default class Rmi extends Base {
     ]],
   });
 
-  execute = () => remove(this.path[0], this.context);
+  execute = async () => {
+    const ids = (await this.execResult('docker', 'images', '-qf', 'dangling=true'))
+      .stdout.toString()
+      .split('\n')
+      .filter(Boolean);
+
+    if (ids.length === 0) {
+      const { stdout } = this.context;
+
+      stdout.write('No none-images need to be removed.\n');
+      return;
+    }
+
+    await spawn.sync('docker', 'rmi', ...ids);
+  };
 }
