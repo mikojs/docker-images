0b258694b0b7bdd1ecb875a96e4bfbc3d2898d56
HsuTing <hsuting0106@gmail.com>
2022-08-25 21:28:46 +0000

    feat(docker-images): add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+const isProduction = process.env.NODE_ENV === 'production';
 const binFolder = path.resolve(__dirname, './src/bin');
 const entry = fs.readdirSync(binFolder)
   .reduce(
     (result, filePath) => {
       const name = filePath.replace(/\.js$/, '');
+
+      if (name === 'pkg') return result;
 
       return {
         ...result,
@@ -16,9 +19,24 @@ const entry = fs.readdirSync(binFolder)
   );
 
 module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  target: 'node',
   entry,
   output: {
     path: path.resolve(__dirname, './docker-images/bin'),
     filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+    ],
   },
 };
