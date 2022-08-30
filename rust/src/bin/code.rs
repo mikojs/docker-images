8715e7c1ed0fddaa09ce139b44ecf7f8955293aa
HsuTing <hsuting0106@gmail.com>
2022-08-30 16:30:34 +0000

    feat(rust): add default code command

--- rust/src/bin/code.rs
+++ rust/src/bin/code.rs
@@ -1,3 +1,15 @@
+use clap::{crate_version, Command};
+
+fn cli() -> Command<'static> {
+    Command::new("code")
+        .version(crate_version!())
+        .about("Use this command to open files in a code-server")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        _ => unreachable!(),
+    }
 }
