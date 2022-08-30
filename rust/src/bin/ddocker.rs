c3120cd7e4f9848d17d1de71b923640360cb5136
HsuTing <hsuting0106@gmail.com>
2022-08-30 16:47:26 +0000

    fix(rust): remove not used function

--- rust/src/bin/ddocker.rs
+++ rust/src/bin/ddocker.rs
@@ -5,8 +5,6 @@ fn cli() -> Command<'static> {
         .version(crate_version!())
         .about("Some docker commands are used in a docker container")
         .subcommand_required(true)
-        .arg_required_else_help(true)
-        .allow_external_subcommands(true)
 }
 
 fn main() {
