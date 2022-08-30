244cf132401d476fcb35a70af048968dd3ae05f7
HsuTing <hsuting0106@gmail.com>
2022-08-30 16:26:28 +0000

    feat(rust): add default ddocker

--- rust/src/bin/ddocker.rs
+++ rust/src/bin/ddocker.rs
@@ -1,3 +1,18 @@
+use clap::{crate_version, Command};
+
+fn cli() -> Command<'static> {
+    Command::new("ddocker")
+        .version(crate_version!())
+        .about("Some commands are used in the docker-in-docker")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        _ => unreachable!(),
+    }
 }
