a2b89666f61424c379240b92b7b822805cea4dd8
HsuTing <hsuting0106@gmail.com>
2022-09-02 08:50:46 +0000

    feat(rust): add rmi command

---
+++ rust/src/rmi.rs
@@ -0,0 +1,30 @@
+use clap::Command;
+
+#[path = "./utils/sub_process.rs"] mod sub_process;
+
+pub fn command() -> Command<'static> {
+    return Command::new("rmi")
+        .about("Find the all ids of the none-images and remove them")
+}
+
+pub fn execute() {
+    let stdout = sub_process::exec_result(
+        "docker",
+        &["images", "-qf", "dangling=true"],
+    );
+    let ids: Vec<&str> = stdout.split("\n")
+        .filter(|x| !x.is_empty())
+        .collect();
+
+    if ids.len() == 0 {
+        println!("No none-images need to be removed.");
+        return;
+    }
+
+    let status = sub_process::exec(
+        "docker",
+        [vec!["rmi"], ids].concat().as_slice(),
+    );
+
+    assert!(status.success());
+}
