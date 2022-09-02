1405c54e166f01c8ce87361389f22a7d50968dd2
HsuTing <hsuting0106@gmail.com>
2022-09-02 08:51:48 +0000

    feat(rust): add rmi command

--- rust/src/bin/ddocker.rs
+++ rust/src/bin/ddocker.rs
@@ -1,6 +1,7 @@
 use clap::{crate_version, Command};
 
 #[path = "../rm.rs"] mod rm;
+#[path = "../rmi.rs"] mod rmi;
 
 fn cli() -> Command<'static> {
     return Command::new("ddocker")
@@ -8,11 +9,13 @@ fn cli() -> Command<'static> {
         .about("Some docker commands are used in a docker container")
         .subcommand_required(true)
         .subcommand(rm::command())
+        .subcommand(rmi::command())
 }
 
 fn main() {
     match cli().get_matches().subcommand() {
         Some(("rm", _)) => rm::execute(),
+        Some(("rmi", _)) => rmi::execute(),
         _ => unreachable!(),
     }
 }
