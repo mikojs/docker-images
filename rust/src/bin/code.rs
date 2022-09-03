66434463175533c6ada8b41498598fbe7814360b
HsuTing <hsuting0106@gmail.com>
2022-09-03 08:29:08 +0000

    feat(rust): could get files

--- rust/src/bin/code.rs
+++ rust/src/bin/code.rs
@@ -1,14 +1,21 @@
 use clap::{crate_version, Command};
 
+#[allow(dead_code)]
+#[path = "../utils/args.rs"] mod args;
+
 fn cli() -> Command<'static> {
     Command::new("code")
         .version(crate_version!())
         .about("Use this command to open files in a code-server")
-        .arg_required_else_help(true)
+        .arg(args::set_proxy_arg())
 }
 
 fn main() {
-    match cli().get_matches().subcommand() {
-        _ => unreachable!(),
-    }
+    let files: Vec<String> = cli()
+        .get_matches()
+        .remove_many("args")
+        .expect("`args` is required")
+        .collect();
+
+    println!("{:?}", files);
 }
