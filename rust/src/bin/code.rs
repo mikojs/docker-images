878dc55e5760da4f057950ea5bc140e3594a4bbe
HsuTing <hsuting0106@gmail.com>
2022-09-03 08:54:20 +0000

    feat(rust): could find the files

--- rust/src/bin/code.rs
+++ rust/src/bin/code.rs
@@ -1,7 +1,14 @@
 use clap::{crate_version, Command};
+use glob;
 
 #[allow(dead_code)]
 #[path = "../utils/args.rs"] mod args;
+
+const OPTIONS: glob::MatchOptions = glob::MatchOptions {
+    case_sensitive: false,
+    require_literal_separator: false,
+    require_literal_leading_dot: false,
+};
 
 fn cli() -> Command<'static> {
     Command::new("code")
@@ -10,12 +17,22 @@ fn cli() -> Command<'static> {
         .arg(args::set_proxy_arg())
 }
 
+fn find_files(pattern: &str) {
+    for entry in glob::glob_with(pattern, OPTIONS).unwrap() {
+        if let Ok(path) = entry {
+            println!("{:?}", path.display())
+        }
+    }
+}
+
 fn main() {
-    let files: Vec<String> = cli()
+    let patterns: Vec<String> = cli()
         .get_matches()
         .remove_many("args")
         .expect("`args` is required")
         .collect();
 
-    println!("{:?}", files);
+    for pattern in patterns {
+        find_files(&pattern);
+    }
 }
