b868940ff2789e7945fecccf5d804435f8460edb
HsuTing <hsuting0106@gmail.com>
2022-09-06 17:24:30 +0000

    perf(version): could accept multiple names to parse env variables

--- src/version.rs
+++ src/version.rs
@@ -1,24 +1,28 @@
-use clap::{Command, Arg, ArgMatches};
+use clap::{Command, ArgMatches};
 
+#[allow(dead_code)]
+#[path = "./utils/args.rs"] mod args;
 #[path = "./utils/parser.rs"] mod parser;
 
 pub fn command() -> Command<'static> {
     Command::new("version")
-        .about("Use this command to parse the docker version from the env variables")
-        .arg(
-            Arg::new("name")
-                .required(true)
-        )
+        .about(r#"Use this command to parse the docker version from the env variables
+The arguments could be the env variable names or the parser names"#)
+        .arg(args::set_proxy_arg())
 }
 
 pub fn execute(sub_matches: &ArgMatches) {
-    println!(
-        "{}",
-        parser::get_env_value(
-            &sub_matches
-                .value_of("name")
-                .expect("Couldn't parse the name from the arguments")
-                .to_string(),
-        ),
-    );
+    let names: Vec<&str> = sub_matches
+        .values_of("args")
+        .unwrap()
+        .collect();
+
+    for name in names {
+        let version = parser::get_env_value(name);
+
+        if !version.is_empty() {
+            println!("{}", version);
+            return;
+        }
+    }
 }
