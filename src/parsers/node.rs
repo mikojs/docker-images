7afac5552001562a17871dc0f561e56bc45156c5
HsuTing <hsuting0106@gmail.com>
2022-09-06 23:09:46 +0000

    feat(node-parser): could find package.json

--- src/parsers/node.rs
+++ src/parsers/node.rs
@@ -1,4 +1,35 @@
-// FIXME: should parse package.json
+use std::env;
+use std::path::{Path, PathBuf};
+
+fn find_package_json(cwd: PathBuf) -> PathBuf {
+    let file_path = cwd.join("package.json");
+
+    if !file_path.exists() {
+        if cwd == Path::new("/") {
+            return Path::new("")
+                .to_path_buf();
+        }
+
+        return find_package_json(
+            cwd.parent()
+                .expect("couldn't find the parent directory")
+                .to_path_buf(),
+        );
+    }
+
+    file_path
+}
+
 fn main() {
-  println!("1.0.0");
+    let cwd = env::current_dir()
+        .expect("Couldn't get the currenct directory");
+    let package_json_path = find_package_json(cwd)
+        .display()
+        .to_string();
+
+    if package_json_path.is_empty() {
+        return;
+    }
+
+    println!("{}", package_json_path);
 }
