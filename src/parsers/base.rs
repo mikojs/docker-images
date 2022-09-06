e1db6a38216f4ac066a31aca7248d89183ce3388
HsuTing <hsuting0106@gmail.com>
2022-09-06 15:44:11 +0000

    feat(utils): add parser util

--- src/parsers/base.rs
+++ src/parsers/base.rs
@@ -2,6 +2,8 @@ use std::env;
 
 use clap::{crate_version, Command, Arg};
 
+#[path = "../utils/parser.rs"] mod parser;
+
 fn cli() -> Command<'static> {
     Command::new("base-parser")
         .version(crate_version!())
@@ -13,15 +15,11 @@ fn cli() -> Command<'static> {
 }
 
 fn main() {
-    let matches = cli()
-        .get_matches();
-    let name = matches
-        .value_of("name")
-        .expect("Couldn't parse the name in the args")
-        .to_string()
-        .to_uppercase();
-
-    if let Ok(value) = env::var(format!("DOCKER_{}_VERSION", name)) {
-        println!("{:?}", value);
-    }
+    parser::print_env_value(
+        &cli()
+            .get_matches()
+            .value_of("name")
+            .expect("Couldn't parse the name in the args")
+            .to_string(),
+    );
 }
