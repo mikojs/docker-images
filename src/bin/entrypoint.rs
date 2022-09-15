54487ee1d05a2d8c236d2961157bafe25377f8a2
HsuTing <hsuting0106@gmail.com>
2022-09-15 09:35:39 +0000

    feat(entrypoint): could get each command

--- src/bin/entrypoint.rs
+++ src/bin/entrypoint.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("hello world");
+    let mut args: Vec<String> = env::args()
+        .collect();
+    let commands_str = args[1].clone();
+
+    args.remove(0);
+    args.remove(0);
+
+    let commands = shellwords::split(&commands_str)
+      .expect("Couldn't get the commands");
+
+    println!("{:?}", commands);
+    println!("{:?}, {}", args, args.len());
 }
