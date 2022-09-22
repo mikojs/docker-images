e35f1ee0f0f53eeb941cea0b023d2eedf33cd23c
HsuTing <hsuting0106@gmail.com>
2022-09-22 01:39:31 +0000

    feat(commands): add psql command

--- src/bin/dpsql.rs
+++ src/bin/dpsql.rs
@@ -1,10 +1,48 @@
+use std::env;
+
 use clap::{crate_version, Command};
+use regex::Regex;
+
+#[path = "../psql.rs"] mod psql;
+
+fn get_db_names() -> Vec<String> {
+    let db_regex = Regex::new(r"DB_URL$")
+        .unwrap();
+    let mut db_names = vec![];
+
+    for (key, _) in env::vars() {
+        if db_regex.is_match(&key) {
+            db_names.push(
+                key
+                    .replace("_DB_URL", "")
+                    .replace("_", " ")
+                    .to_lowercase(),
+            );
+        }
+    }
+
+    db_names
+}
 
 fn main() {
-    let matches = Command::new("dpsql")
+    let mut app = Command::new("dpsql")
         .version(crate_version!())
-        .about("Use psql command in the docker container")
-        .get_matches();
+        .about("Use psql command in the docker container");
 
-    println!("{:?}", matches);
+    for db_name in get_db_names() {
+        app = app.subcommand(psql::command(&db_name));
+    }
+
+    let matches = app.get_matches();
+
+    for (sub_command, sub_matches) in matches.subcommand() {
+        for db_name in get_db_names() {
+            if sub_command == db_name {
+                psql::execute(sub_matches);
+                return;
+            }
+        }
+    }
+
+    println!("TODO: use docker run");
 }
---
+++ src/psql.rs
@@ -0,0 +1,18 @@
+use clap::{Command, ArgMatches};
+
+#[allow(dead_code)]
+#[path = "./utils/args.rs"] mod args;
+
+pub fn command(db_name: &str) -> Command<'static> {
+    Command::new(db_name)
+        // TODO: .about(&format!("Connect to `{}`", db_name))
+        // FIXME: args isn't required
+        .arg(args::set_proxy_arg())
+}
+
+pub fn execute(sub_matches: &ArgMatches) {
+    println!(
+        "{:?}",
+        args::get_values_from_args(sub_matches),
+    );
+}
