fcda90377238aa8dd003d648de4908dea2f09a9b
HsuTing <hsuting0106@gmail.com>
2022-09-22 15:02:45 +0000

    refactor(psql): remove psql_show

--- src/psql.rs
+++ src/psql.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::process;
 
 use clap::{App, Command, ArgMatches};
@@ -7,46 +8,52 @@ use clap::{App, Command, ArgMatches};
 
 #[allow(dead_code)]
 #[path = "./run.rs"] mod run;
-#[path = "./psql_show.rs"] mod psql_show;
 #[path = "./psql_clone.rs"] mod psql_clone;
+
+fn get_db_url(db_name: &str) -> String {
+    let db_env_name = format!("{}_DB_URL", db_name.to_uppercase());
+
+    if let Ok(db_url) = env::var(&db_env_name) {
+        return db_url;
+    }
+
+    eprint!(
+        "`{}` isn't in the environment variables.",
+        db_env_name,
+    );
+    process::exit(1);
+}
 
 pub fn command(app: App<'static>) -> Command<'static> {
     app
-        .subcommand(psql_show::command())
+        .subcommand(
+            Command::new("show")
+                .about("Show the database url")
+        )
         .subcommand(psql_clone::command())
         .arg(args::set_proxy_arg(false))
 }
 
 pub fn execute(sub_matches: &ArgMatches, db_name: &str) {
-    let db_url = psql_show::execute(db_name);
+    let db_url = get_db_url(db_name);
 
     match sub_matches.subcommand() {
         Some(("show", _)) => println!("{}", db_url),
-        Some(("clone", _)) => psql_clone::execute(db_url),
-        _ => {
-            if db_url.is_empty() {
-                eprint!(
-                    "`{}` isn't in the environment variables.",
-                    db_name.to_uppercase(),
-                );
-                process::exit(1);
-            }
-
-            run::execute(
-                &args::generate_arg_matches(
-                    [
-                        vec![
-                            "-it",
-                            "--rm",
-                            "postgres:alpine",
-                            "psql",
-                            &db_url,
-                        ],
-                        args::get_values_from_args(sub_matches),
-                    ]
-                        .concat(),
-                ),
-            );
-        },
+        Some(("clone", _)) => psql_clone::execute(&db_url),
+        _ => run::execute(
+            &args::generate_arg_matches(
+                [
+                    vec![
+                        "-it",
+                        "--rm",
+                        "postgres:alpine",
+                        "psql",
+                        &db_url,
+                    ],
+                    args::get_values_from_args(sub_matches),
+                ]
+                    .concat(),
+            ),
+        ),
     }
 }
