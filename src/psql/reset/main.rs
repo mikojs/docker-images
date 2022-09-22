36c72f561cc59850199fb096dd60d082f2b2bacb
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2022-09-23 06:06:34 +0800

    Could reset some data in database (#54)
    * feat(psql): add truncate command
    
    * feat(psql): add reset command
    
    * chore(psql): move truncate to table
    
    * feat(psql): could reset sequence

---
+++ src/psql/reset/main.rs
@@ -0,0 +1,20 @@
+use clap::{Command, ArgMatches};
+
+#[path = "./table.rs"] mod table;
+#[path = "./sequence.rs"] mod sequence;
+
+pub fn command() -> Command<'static> {
+    Command::new("reset")
+        .about("Reset something in the database")
+        .subcommand_required(true)
+        .subcommand(table::command())
+        .subcommand(sequence::command())
+}
+
+pub fn execute(matches: &ArgMatches, db_url: &str) {
+    match matches.subcommand() {
+        Some(("table", sub_matches)) => table::execute(sub_matches, db_url),
+        Some(("sequence", sub_matches)) => sequence::execute(sub_matches, db_url),
+        _ => unreachable!(),
+    }
+}
