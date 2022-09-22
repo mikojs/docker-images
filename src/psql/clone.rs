0f3c58a912f0518f951d5803e64ee09a4c92d6f7
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2022-09-23 07:31:36 +0800

    Could select table when cloning data (#55)
    * feat(clone): could select a table
    
    * perf(args): move filter_args to utils
    
    * feat(psql): could clone the specific table

--- src/psql/clone.rs
+++ src/psql/clone.rs
@@ -7,6 +7,14 @@ use clap::{Command, Arg, ArgMatches};
 
 #[path = "./utils.rs"] mod utils;
 
+fn get_table_name(matches: &ArgMatches) -> String {
+    if let Some(table_name) = matches.value_of("table-name") {
+        return table_name.to_string();
+    }
+
+    "".to_string()
+}
+
 pub fn command() -> Command<'static> {
     Command::new("clone")
         .about("Clone the database from the database url")
@@ -14,24 +22,39 @@ pub fn command() -> Command<'static> {
             Arg::new("file-name")
                 .required(true)
         )
+        .arg(
+            Arg::new("table-name")
+                .short('t')
+                .long("table")
+                .help("Choose a table")
+                .takes_value(true)
+        )
 }
 
 pub fn execute(matches: &ArgMatches, db_url: &str) {
     utils::check_db_url(db_url);
     run::execute(
         &args::generate_arg_matches(
-            vec![
-                "-it",
-                "--rm",
-                "postgres:alpine",
-                "pg_dump",
-                "-Fc",
-                "-f",
-                matches
-                    .value_of("file-name")
-                    .unwrap(),
-                db_url,
-            ],
+            [
+                vec![
+                    "-it",
+                    "--rm",
+                    "postgres:alpine",
+                    "pg_dump",
+                    "-Fc",
+                ],
+                args::filter_args(
+                    vec!["-t", &get_table_name(matches)],
+                ),
+                vec![
+                    "-f",
+                    matches
+                        .value_of("file-name")
+                        .unwrap(),
+                    db_url,
+                ],
+            ]
+                .concat(),
         ),
     );
 }
