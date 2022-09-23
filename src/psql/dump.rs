4482f070c1b62c7453745b41627376ce911bd7a4
Ting-Hsiang Hsu <hsuting0106@gmail.com>
2022-09-23 17:59:39 +0800

    psql could use proxy args (#58)
    * chore(psql): rename file name
    
    * perf(psql): remove table-name, but support proxy args
    
    * fix(psql): fix detail
    
    * feat(psql): restore command support proxy arg
    
    * refactor(psql): no longer need to parse table name

--- src/psql/dump.rs
+++ src/psql/dump.rs
@@ -8,28 +8,14 @@ use clap::{Command, Arg, ArgMatches};
 
 #[path = "./utils/check_db_url.rs"] mod check_db_url;
 
-fn get_table_name(matches: &ArgMatches) -> String {
-    if let Some(table_name) = matches.value_of("table-name") {
-        return table_name.to_string();
-    }
-
-    "".to_string()
-}
-
 pub fn command() -> Command<'static> {
-    Command::new("clone")
-        .about("Clone the database from the database url")
+    Command::new("dump")
+        .about("Dump the database to a file")
         .arg(
             Arg::new("file-name")
                 .required(true)
         )
-        .arg(
-            Arg::new("table-name")
-                .short('t')
-                .long("table")
-                .help("Choose a table")
-                .takes_value(true)
-        )
+        .arg(args::set_proxy_arg(true))
 }
 
 pub fn execute(matches: &ArgMatches, db_name: &str, db_url: &str) {
@@ -43,17 +29,13 @@ pub fn execute(matches: &ArgMatches, db_name: &str, db_url: &str) {
                     "postgres:alpine",
                     "pg_dump",
                     "-Fc",
-                ],
-                args::filter_args(
-                    vec!["-t", &get_table_name(matches)],
-                ),
-                vec![
                     "-f",
                     matches
                         .value_of("file-name")
                         .unwrap(),
                     db_url,
                 ],
+                args::get_values_from_args(matches),
             ]
                 .concat(),
         ),
--- src/psql/restore.rs
+++ src/psql/restore.rs
@@ -1,5 +1,7 @@
 use clap::{Command, Arg, ArgMatches};
 
+#[allow(dead_code)]
+#[path = "../utils/args.rs"] mod args;
 #[path = "../utils/generate_arg_matches.rs"] mod generate_arg_matches;
 #[allow(dead_code)]
 #[path = "../run.rs"] mod run;
@@ -8,30 +10,35 @@ use clap::{Command, Arg, ArgMatches};
 
 pub fn command() -> Command<'static> {
     Command::new("restore")
-        .about("Restore the database from the cloned database file")
+        .about("Restore the database from a file")
         .arg(
             Arg::new("file-name")
                 .required(true)
         )
+        .arg(args::set_proxy_arg(true))
 }
 
 pub fn execute(matches: &ArgMatches, db_name: &str, db_url: &str) {
     check_db_url::main(db_name, db_url, false);
     run::execute(
         &generate_arg_matches::main(
-            vec![
-                "-it",
-                "--rm",
-                "postgres:alpine",
-                "pg_restore",
-                "--no-owner",
-                "-x",
-                "-d",
-                db_url,
-                matches
-                    .value_of("file-name")
-                    .unwrap(),
-            ],
+            [
+                vec![
+                    "-it",
+                    "--rm",
+                    "postgres:alpine",
+                    "pg_restore",
+                    "--no-owner",
+                    "-x",
+                    "-d",
+                    db_url,
+                    matches
+                        .value_of("file-name")
+                        .unwrap(),
+                ],
+                args::get_values_from_args(matches),
+            ]
+                .concat(),
         ),
     );
 }
