604ce209d6ccf0718495a74d2d48d48363e796eb
HsuTing <hsuting0106@gmail.com>
2022-09-23 09:21:11 +0000

    feat(psql): if db is protected, leave directly

--- src/psql/utils/check_db_url.rs
+++ src/psql/utils/check_db_url.rs
@@ -5,12 +5,10 @@ use inquire::Confirm;
 
 fn is_protected_db(db_name: &str) -> bool {
     if let Ok(not_protected_db_names_str) = env::var("NOT_PROTECTED_DBS") {
-        let not_protected_db_names: Vec<&str> = not_protected_db_names_str
+        return not_protected_db_names_str
             .split(",")
-            .filter(|x| !x.is_empty())
-            .collect();
-
-        println!("{:?}", not_protected_db_names);
+            .find(|&x| x == db_name)
+            .is_none();
     }
 
     true
@@ -19,7 +17,7 @@ fn is_protected_db(db_name: &str) -> bool {
 pub fn main(db_name: &str, db_url: &str, skip_protected_db_checking: bool) -> bool {
     if !skip_protected_db_checking && is_protected_db(db_name) {
         eprint!("The `{}` database is protected", db_name);
-        process::exit(0);
+        process::exit(1);
     }
 
     let message = format!("Use `{}`. Do you want to continue or not:", db_url);
