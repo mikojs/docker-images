79a4e7d82216fa15c604a31cbf7a4c1386fa1bd0
HsuTing <hsuting0106@gmail.com>
2022-09-24 13:00:27 +0000

    feat(utils): add docker_run_with_image

---
+++ src/utils/docker_run_with_image.rs
@@ -0,0 +1,36 @@
+use std::env;
+
+#[path = "./docker_run.rs"] mod docker_run;
+
+fn get_image(image_name: &str, env_name: &str, values: Vec<&str>) -> String {
+    let docker_env_name = format!(
+        "DOCKER_{}_VERSION",
+        env_name.to_uppercase(),
+    );
+
+    if let Ok(env) = env::var(&docker_env_name) {
+        return format!("{}:{}", image_name, env);
+    }
+
+    for value in values {
+        if !value.is_empty() {
+            return format!("{}:{}", image_name, value);
+        }
+    } 
+
+    format!("{}:alpine", image_name)
+}
+
+pub fn main(args: Vec<&str>) {
+    docker_run::main(
+        [
+            vec![
+                "-it",
+                "--rm",
+                get_image("postgres", "postgres", vec!["alpine"]),
+            ],
+            args,
+        ]
+            .concat(),
+    );
+}
