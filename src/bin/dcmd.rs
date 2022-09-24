573ef847c58e6cefff75d22c51196b1b68f4edf4
HsuTing <hsuting0106@gmail.com>
2022-09-24 14:51:07 +0000

    feat(dcmd): add new command

--- src/bin/dcmd.rs
+++ src/bin/dcmd.rs
@@ -1,3 +1,26 @@
+use std::env;
+
+use clap::{crate_version, Command, Arg};
+
+#[path = "../utils/proxy_args.rs"] mod proxy_args;
+#[path = "../utils/docker_run_with_image.rs"] mod docker_run_with_image;
+
 fn main() {
-    println!("hello world");
+    let matches = Command::new("dcmd")
+        .version(crate_version!())
+        .about("Run docker with the DOCKER_NAME_VERSION environment variable")
+        .arg(
+            Arg::new("image-name")
+                .required(true)
+        )
+        .arg(proxy_args::set_proxy_args(false))
+        .get_matches();
+    let docker_image = matches.value_of("image-name")
+        .unwrap();
+
+    docker_run_with_image::main(
+        docker_image,
+        vec![&format!("DOCKER_{}_VERSION", docker_image.to_uppercase())],
+        proxy_args::get_values_from_proxy_args(&matches)
+    );
 }
