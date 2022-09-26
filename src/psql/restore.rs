252ff1c6a73c3f6d95c1de1da0ecf1b71f86b88c
HsuTing <hsuting0106@gmail.com>
2022-09-26 09:09:30 +0000

    feat(psql): restore command support to restore from csv file

--- src/psql/restore.rs
+++ src/psql/restore.rs
@@ -1,4 +1,7 @@
+use std::process;
+
 use clap::{Command, Arg, ArgMatches};
+use regex::Regex;
 
 #[path = "../utils/proxy_args.rs"] mod proxy_args;
 
@@ -15,6 +18,30 @@ pub fn command() -> Command<'static> {
 }
 
 pub fn execute(matches: &ArgMatches, db_url: &str) {
+    let file_name = matches
+        .value_of("file-name")
+        .unwrap();
+    let is_csv = Regex::new(r"\.csv$")
+        .unwrap()
+        .is_match(file_name);
+    let args = proxy_args::get_values_from_proxy_args(matches);
+
+    if is_csv {
+        if args.len() != 1 {
+            eprint!("If you want to restore data from a CSV file, only one argument about SQL query could be accepted");
+            process::exit(1);
+        }
+
+        docker_run::main(
+            vec![
+                "psql",
+                "-c",
+                &format!("\\copy {} FROM '{}' WITH csv", args[0], file_name),
+            ],
+        );
+        return;
+    }
+
     docker_run::main(
         [
             vec![
@@ -23,11 +50,9 @@ pub fn execute(matches: &ArgMatches, db_url: &str) {
                 "-x",
                 "-d",
                 db_url,
-                matches
-                    .value_of("file-name")
-                    .unwrap(),
+                file_name,
             ],
-            proxy_args::get_values_from_proxy_args(matches),
+            args,
         ]
             .concat(),
     );
