b1139a007d818f81e22b531b45709036bc3a3c90
HsuTing <hsuting0106@gmail.com>
2022-09-26 17:28:26 +0000

    perf(utils): add utils mod

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,5 @@ pub mod run;
 pub mod exec;
 pub mod rm;
 pub mod rmi;
-
 pub mod psql;
-
-pub mod utils {
-    pub mod proxy_args;
-    pub mod sub_process;
-    pub mod get_current_dir;
-}
+pub mod utils;
