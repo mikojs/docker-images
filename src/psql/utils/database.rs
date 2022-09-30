5049cf117996fa9a3f768612da0919cac4b63229
HsuTing <hsuting0106@gmail.com>
2022-09-30 08:26:20 +0000

    fix(psql): should add white space

--- src/psql/utils/database.rs
+++ src/psql/utils/database.rs
@@ -76,12 +76,12 @@ impl Database {
 
     fn is_danger_sql(&self, arg: &str) -> bool {
         let keyword_regexs = vec![
-            Regex::new(r"INSERT"),
-            Regex::new(r"CREATE"),
-            Regex::new(r"UPDATE"),
-            Regex::new(r"DELETE"),
-            Regex::new(r"ALTER"),
-            Regex::new(r"TRUNCATE"),
+            Regex::new(r"INSERT "),
+            Regex::new(r"CREATE "),
+            Regex::new(r"UPDATE "),
+            Regex::new(r"DELETE "),
+            Regex::new(r"ALTER "),
+            Regex::new(r"TRUNCATE "),
         ];
 
         for keyword_regex in &keyword_regexs {
