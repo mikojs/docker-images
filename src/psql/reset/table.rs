ab3c9a8dace86537a3cd526f2fcf4a86d447fd6b
HsuTing <hsuting0106@gmail.com>
2022-10-07 15:11:16 +0000

    feat(dpsql): add delete option to reset table

--- src/psql/reset/table.rs
+++ src/psql/reset/table.rs
@@ -1,4 +1,4 @@
-use clap::{Command, Arg, ArgMatches};
+use clap::{Command, Arg, ArgAction, ArgMatches};
 
 use crate::psql::utils::{docker, Database};
 
@@ -10,16 +10,30 @@ pub fn command() -> Command<'static> {
                 .help("This table would be reset")
                 .required(true)
         )
+        .arg(
+            Arg::new("delete")
+                .help(r#"This command would use `TRUNCATE` by default
+If you want to use `DELETE`, you could use this option"#)
+                .long("delete")
+                .action(ArgAction::SetTrue)
+                .default_value("false")
+        )
 }
 
 pub fn execute(matches: &ArgMatches, db: Database) {
+    let command = match matches.get_one::<bool>("delete") {
+        Some(true) => "DELETE FROM",
+        _ => "TRUNCATE TABLE"
+    };
+
     docker::run(
         vec![
             "psql",
             db.url(true),
             "-c",
             &format!(
-                "TRUNCATE TABLE {};",
+                "{} {};",
+                command,
                 matches
                     .value_of("table-name")
                     .unwrap(),
