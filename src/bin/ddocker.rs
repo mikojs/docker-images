940587073975e85ee2efc177cf79cb00f4f7a8b8
HsuTing <hsuting0106@gmail.com>
2022-10-08 18:07:15 +0000

    fix(ddocker): fix error

--- src/bin/ddocker.rs
+++ src/bin/ddocker.rs
@@ -1,8 +1,10 @@
+use std::io::Error;
+
 use clap::{crate_version, Command};
 
 use docker_images::{name, run, exec, rm, rmi, volume};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let matches = Command::new("ddocker")
         .version(crate_version!())
         .about("Some docker commands are used in a docker container")
@@ -16,12 +18,13 @@ fn main() {
         .get_matches();
 
     match matches.subcommand() {
-        Some(("name", _)) => name::execute(),
-        Some(("run", sub_matches)) => run::execute(sub_matches),
-        Some(("exec", sub_matches)) => exec::execute(sub_matches),
-        Some(("rm", _)) => rm::execute(),
-        Some(("rmi", _)) => rmi::execute(),
-        Some(("volume", sub_matches)) => volume::execute(sub_matches),
+        Some(("name", _)) => name::execute()?,
+        Some(("run", sub_matches)) => run::execute(sub_matches)?,
+        Some(("exec", sub_matches)) => exec::execute(sub_matches)?,
+        Some(("rm", _)) => rm::execute()?,
+        Some(("rmi", _)) => rmi::execute()?,
+        Some(("volume", sub_matches)) => volume::execute(sub_matches)?,
         _ => unreachable!(),
-    }
+    };
+    Ok(())
 }
