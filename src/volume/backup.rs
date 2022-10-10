de233784feef02659eb8bd37f708e8095effcd98
HsuTing <hsuting0106@gmail.com>
2022-10-10 14:30:28 +0000

    feat(ddocker): support to backup multiple volumes to a tar file

--- src/volume/backup.rs
+++ src/volume/backup.rs
@@ -1,36 +1,55 @@
 use std::env;
 
-use clap::{Arg, ArgMatches, Command};
+use clap::{ArgMatches, Command};
 
 use crate::utils::{args, docker, Error};
 
+fn build_volume_args(matches: &ArgMatches) -> Vec<String> {
+    let mut args = vec![];
+
+    for volume_name in args::get_values_from_proxy(matches) {
+        let volume_arg = format!("{0}:/backup/{0}", volume_name);
+
+        args.push("-v".to_string());
+        args.push(volume_arg);
+    }
+
+    args
+}
+
 pub fn command() -> Command<'static> {
-    Command::new("backup").about("Backup a docker volume").arg(
-        Arg::new("volume-name")
-            .help("This docker volume would be backup")
-            .required(true),
-    )
+    Command::new("backup")
+        .about("Backup the docker volumes")
+        .arg(
+            args::set_proxy(true)
+                .value_name("volume-names")
+                .help("Those docker volumes would be backup"),
+        )
 }
 
 pub fn execute(matches: &ArgMatches) -> Result<(), Error> {
-    let volume_name = args::value_of(matches, "volume-name");
-
-    docker::run(vec![
-        "-it",
-        "--rm",
-        "-v",
-        &format!("{0}:/{0}", volume_name),
-        "-w",
-        "/",
-        "docker",
-        "/bin/sh",
-        "-c",
-        &format!(
-            "tar -cvf {0}.tar {0} && docker cp {0}.tar {1}:{2}",
-            volume_name,
-            docker::name()?,
-            env::current_dir()?.display().to_string(),
-        ),
-    ])?;
+    docker::run(
+        [
+            build_volume_args(matches)
+                .iter()
+                .map(AsRef::as_ref)
+                .collect(),
+            vec![
+                "-it",
+                "--rm",
+                "-w",
+                "/",
+                "docker",
+                "/bin/sh",
+                "-c",
+                &format!(
+                    "tar -cvf backup.tar backup && docker cp backup.tar {0}:{1}",
+                    docker::name()?,
+                    env::current_dir()?.display().to_string(),
+                ),
+            ],
+        ]
+        .concat(),
+    )?;
     Ok(())
 }
